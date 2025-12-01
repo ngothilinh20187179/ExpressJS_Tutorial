ed6b6f1dfdd14e3917bf3ee6a23250d6f1814f8a
Ngo Thi Linh <ngothilinh12915@gmail.com>
2025-12-01 13:38:58 +1100

    Middlewares

--- app.js
+++ app.js
@@ -5,15 +5,47 @@ const PORT = 3000;
 const basicRoutes = require('./routing/basic');
 const parameterRoutes = require('./routing/parameters')
 const chaining = require('./routing/chaining');
+const errorHandling = require('./error_handling/basic');
 
-app.use(express.json()); // Middleware to parse JSON bodies
+// Built-in Middleware
+app.use(express.json()); // Parses JSON data for every incoming request
 
-app.use('/basic', basicRoutes); // Test: http://localhost:3000/basic/get-example
-app.use('/parameters', parameterRoutes); // Test: http://localhost:3000/parameters/users/2
-app.use('/chaining', chaining);
+// Application-level Middleware (before any routes)
+app.use((req, res, next) => { // Log for every incoming request
+    console.log('Time:', Date.now());
+    console.log('Request URL:', req.originalUrl);
+    next();
+});
 
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Express Multiple Routes Server!');
+});
+app.use('/basic', basicRoutes);
+app.use('/parameters', parameterRoutes);
+app.use('/chaining', chaining);
+app.use('/error', errorHandling);
+
+// Router-level
+const premiumRoutes = express.Router();
+premiumRoutes.use((req, res, next) => {
+    const user = req.query.user || 'standard';
+    if (user != 'premium') {
+        return res.status(403).send('Access denied. Premium members only.');
+        // http://localhost:3000/premium/content
+    }
+    console.log('Premium Route Accessed:', req.originalUrl);
+    next();
+});
+premiumRoutes.get('/content', (req, res) => {
+    res.send('This is your exclusive content');
+    // http://localhost:3000/premium/content?user=premium
+});
+app.use('/premium', premiumRoutes);
+
+// Error-handling Middleware (ALWAYS AT THE END)
+app.use((err, req, res, next) => {
+    console.log("Test:", err.stack);
 });
 
 app.listen(PORT, () => {
